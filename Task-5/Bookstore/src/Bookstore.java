2fed51b20187634450b5c9d33fd8c0961ff5055f
aleksander tikhonov <77674818+lordsprayer@users.noreply.github.com>
2021-03-02 12:59:59 +0300

    delete old version

--- Task-5/Bookstore/src/Bookstore.java
+++
@@ -1,9 +0,0 @@
-import com.senla.courses.ui.menu.MenuController;
-
-public class Bookstore {
-
-    public static void main(String[] args) {
-        MenuController.getInstance(). run();
-    }
-
-}
