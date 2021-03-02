4c7d4354f251c7e54bdab95a0974952defe3c6af
aleksander tikhonov <77674818+lordsprayer@users.noreply.github.com>
2021-03-02 14:32:47 +0300

    v0.3

--- Task-6/Bookstore/src/com/senla/courses/ui/action/IAction.java
+++
@@ -1,5 +0,0 @@
-package com.senla.courses.ui.action;
-
-public interface IAction {
-    void execute();
-}
--- Task-6/Bookstore/src/com/senla/courses/ui/action/PrintAllBooks.java
+++
@@ -1,9 +0,0 @@
-package com.senla.courses.ui.action;
-
-public class PrintAllBooks extends AbstractAction implements IAction{
-
-    @Override
-    public void execute() {
-        facade.printAllBook();
-    }
-}
