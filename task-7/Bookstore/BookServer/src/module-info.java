1c2dd460a8ded71d30dde224d77a3d4c37b19fae
aleksander tikhonov <77674818+lordsprayer@users.noreply.github.com>
2021-04-01 12:20:11 +0300

    Создание ветки

--- task-7/Bookstore/BookServer/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module bookServer {
-    requires java.logging;
-    exports com.senla.courses.facade;
-    exports com.senla.courses.model;
-    exports com.senla.courses.config;
-    exports com.senla.courses.exception;
-    exports com.senla.courses.util;
-}
