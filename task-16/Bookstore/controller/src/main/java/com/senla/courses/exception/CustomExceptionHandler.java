8900880f9e3d87521fd3e1e0a2a48af0f6f261f4
aleksander tikhonov <tihon2209@mail.ru>
2021-05-23 14:47:14 +0300

    Task-16:editing CustomExceptionHandler

--- task-16/Bookstore/controller/src/main/java/com/senla/courses/exception/CustomExceptionHandler.java
+++ task-16/Bookstore/controller/src/main/java/com/senla/courses/exception/CustomExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.senla.courses.config;
+package com.senla.courses.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler(DaoException.class)
+    public ResponseEntity<String> handlerDaoException(DaoException ex) {
+        return new ResponseEntity<>(ex.getLocalizedMessage(), HttpStatus.I_AM_A_TEAPOT);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handlerDaoException(Exception ex) {
         return new ResponseEntity<>(ex.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
